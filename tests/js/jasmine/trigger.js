14d48b40f32dc8f3ade4956523ab43646a557a08
Reetesh Ranjan <reeteshranjan@piaxis.tech>
2017-08-04 13:31:37 +0530

    Github pages

--- tests/js/jasmine/trigger.js
+++
@@ -1,7 +0,0 @@
-describe('trigger', function() {
-
-  it('should throw an error for unsupported event type', function() {
-    expect(function(){$_(document.body).trigger('mousemove')}).toThrow(new Error('Unsupported event mousemove'))
-  })
-
-})
